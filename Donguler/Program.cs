b366a27c3f6c0113f930db2462144f310f7c7109
Çetin Ürün <cetinurun.12@outlook.com>
2021-03-15 23:20:39 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ProductName = "Laptop";
+            product1.ProductValue = 4500;
+            product1.StockAmount = 28;
+
+            Product product2 = new Product();
+            product2.ProductName = "Desk";
+            product2.ProductValue = 700;
+            product2.StockAmount = 17;
+
+            Product product3 = new Product();
+            product3.ProductName = "Smart Phone";
+            product3.ProductValue = 4500;
+            product3.StockAmount = 6;
+
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ProductName + " " + product.ProductValue);
+            }
+            Console.WriteLine("------------------------");
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].ProductName + " " + products[i].ProductValue);
+            }
+            Console.WriteLine("------------------------");
+
+            int j = products;
+            while (j<products.Length)
+            {
+                Console.WriteLine(products[j].ProductName + " " + products[j].ProductValue);
+                j++;
+            }
+
+        }
+    }
+    }
+
+    class Product
+    {
+        public string ProductName { get; set; }
+        public int ProductValue { get; set; }
+        public int StockAmount { get; set; }
+    }
---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    //İş sınıfları
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
